372ad4eea2c9291d9d2a0351f97d58bbfb820af7
João Matos <joao.matos.jm@gmail.com>
2021-04-06 04:34:33 +0100

    Added input splitting

--- se1/main.c
+++ se1/main.c
@@ -3,12 +3,14 @@
 #include <string.h>
 
 #define MAX_CMD_LEN 2024
+#define MAX_ARGS_AMOUNT 24
 
 int main() {
     char userInput[MAX_CMD_LEN];
     for (;;) {
         printf("$ ");
 
+        ///Initial Checks
         if (fgets(userInput, MAX_CMD_LEN, stdin) == NULL)
             exit(1);
         size_t len = strlen(userInput);
@@ -17,7 +19,20 @@ int main() {
         if (strcmp(userInput, "exit") == 0)
             break;
 
-        printf("%s\n", userInput);
+        ///Split Filename
+        char *fileExit;
+        strtok_r(userInput, ">", &fileExit);
+        if (fileExit[0] == '\0')
+            fileExit = 0;
+        puts(fileExit);
+
+        ///Split Arguments
+        char *args[MAX_ARGS_AMOUNT];
+        args[0] = strtok(userInput, " ");
+        for (int i = 0; args[i] != NULL; i++) {
+            args[i + 1] = strtok(NULL, " ");
+            puts(args[i]);
+        }
     }
     return 0;
 }
